eb5d90da9a9803edcca194649eabdf0d85fa55da
Fellyx <Fellyx@users.noreply.github.com>
2022-04-28 13:23:54 -0300

    Git Init
    Initializing the git repository.

---
+++ src/main/java/com/example/SpringConsumingRestAPI/SpringConsumingRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringConsumingRestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConsumingRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConsumingRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringConsumingRestAPI/SpringConsumingRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringConsumingRestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConsumingRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
