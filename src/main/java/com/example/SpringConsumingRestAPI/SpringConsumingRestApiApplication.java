67fdb20b349d9742ee14f02a0c210ec8aaf5b0ca
Fellyx <Fellyx@users.noreply.github.com>
2022-05-02 10:21:50 -0300

    Creating the Quote and Value classes
    Creating the Value and Quote classes,
    that is goona receive the values and
    quotes from the API towards get
    request.'

--- src/main/java/com/example/SpringConsumingRestAPI/SpringConsumingRestApiApplication.java
+++ src/main/java/com/example/SpringConsumingRestAPI/SpringConsumingRestApiApplication.java
@@ -1,13 +1,30 @@
 package com.example.SpringConsumingRestAPI;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
 
 @SpringBootApplication
 public class SpringConsumingRestApiApplication {
 
-	public static void main(String[] args) {
+	private static final Logger log = 
+	    LoggerFactory.getLogger(SpringConsumingRestApiApplication.class);  
+  
+  public static void main(String[] args) {
 		SpringApplication.run(SpringConsumingRestApiApplication.class, args);
 	}
+  
+  @Bean
+  public RestTemplate restTemplate(RestTemplateBuilder builder) {
+    return builder.build();
+  }
+  
+  
 
 }
